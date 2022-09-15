6f7e90cdc9a6fb07a34def504ca61199377e0efe
AlinaMytsa <act.mitsa@gmail.com>
2022-09-15 17:30:49 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,32 +1,69 @@
 "use strict";
 
 (function (){
- const listData = [1, 2, [1.1, 1.2, 1.3] , 3];
- const mainList = document.createElement('ul');
+  const listData = [1, 2, [1.1, 1.2, [2.4, 5.6], 1.3] , 3]
 
- for (let i = 0; i < listData.length; i += 1){
-   const li = document.createElement('li');
-   mainList.append(li);
+  const recursiveList = (data) => {
+    const ul = document.createElement('ul');
 
-   if (Array.isArray(listData[i])) {
+    for (let i = 0; i < data.length; i += 1){
 
-     const secondaryList = document.createElement('ul');
+      const li = document.createElement('li');
+      if (Array.isArray(data[i])){
+       li.append( recursiveList(data[i]) );
+      } else {
+        li.innerHTML = data[i];
+      }
 
-     for (let j = 0; j < listData[i].length; j += 1) {
+      ul.append(li);
 
-       const secondaryLi = document.createElement('li');
-       secondaryLi.innerHTML = listData[i][j];
+    }
+  return ul;
 
-       secondaryList.append(secondaryLi);
-     }
+  }
 
-     li.append(secondaryList);
+  const ul = recursiveList(listData);
 
-   } else {
-     li.innerHTML = listData[i];
-   }
+  document.body.append(ul);
 
-   mainList.append(li);
- }
- document.body.append(mainList);
 })()
+
+
+
+
+
+
+
+
+
+
+// (function (){
+//  const listData = [1, 2, [1.1, 1.2, 1.3] , 3];
+//  const mainList = document.createElement('ul');
+//
+//  for (let i = 0; i < listData.length; i += 1){
+//    const li = document.createElement('li');
+//    mainList.append(li);
+//
+//    if (Array.isArray(listData[i])) {
+//
+//      const secondaryList = document.createElement('ul');
+//
+//      for (let j = 0; j < listData[i].length; j += 1) {
+//
+//        const secondaryLi = document.createElement('li');
+//        secondaryLi.innerHTML = listData[i][j];
+//
+//        secondaryList.append(secondaryLi);
+//      }
+//
+//      li.append(secondaryList);
+//
+//    } else {
+//      li.innerHTML = listData[i];
+//    }
+//
+//    mainList.append(li);
+//  }
+//  document.body.append(mainList);
+// })()
